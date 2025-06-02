ec4c9e2fbd23829366433f0a450e9ebaba466464
K1ngOfPugs <73603426+K1ngOfPugs@users.noreply.github.com>
2025-06-02 10:56:41 -0400

    Update mac-post.sh

--- .github/workflows/mac-post.sh
+++ .github/workflows/mac-post.sh
@@ -1,24 +1,35 @@
-#!/usr/bin/env zsh
+et -euo pipefail
 
-set -e
-export WORKFLOW_ROOT=/Users/runner/work/futurerestore/futurerestore/.github/workflows
-export DEP_ROOT=/Users/runner/work/futurerestore/futurerestore/dep_root
-export BASE=/Users/runner/work/futurerestore/futurerestore/
+cd "$BASE"
 
-cd ${BASE}
-export FUTURERESTORE_VERSION=$(git rev-list --count HEAD | tr -d '\n')
-export FUTURERESTORE_VERSION_SHA=$(git rev-parse HEAD | tr -d '\n')
-export FUTURERESTORE_VERSION_RELEASE=$(cat version.txt | tr -d '\n')
-echo -n $FUTURERESTORE_VERSION_SHA > latest_build_sha.txt
-echo -n $FUTURERESTORE_VERSION > latest_build_num.txt
-lipo -create -arch x86_64 cmake-build-release-x86_64/src/futurerestore -arch arm64 cmake-build-release-arm64/src/futurerestore -output futurerestore
-codesign --force --sign - futurerestore
-tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-RELEASE.tar.xz" futurerestore
-rm -rf futurerestore
-lipo -create -arch x86_64 cmake-build-debug-x86_64/src/futurerestore -arch arm64 cmake-build-debug-arm64/src/futurerestore -output futurerestore
-codesign --force --sign - futurerestore
-tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-DEBUG.tar.xz" futurerestore
-rm -rf futurerestore
-lipo -create -arch x86_64 cmake-build-asan-x86_64/src/futurerestore -arch arm64 cmake-build-asan-arm64/src/futurerestore -output futurerestore
-codesign --force --sign - futurerestore
-tar cpPJf "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-ASAN.tar.xz" futurerestore
+export FUTURERESTORE_VERSION=$(git rev-list --count HEAD)
+export FUTURERESTORE_VERSION_SHA=$(git rev-parse HEAD)
+export FUTURERESTORE_VERSION_RELEASE=$(< version.txt)
+
+print -n $FUTURERESTORE_VERSION_SHA > latest_build_sha.txt
+print -n $FUTURERESTORE_VERSION > latest_build_num.txt
+
+function package() {
+  local x86_bin=$1
+  local arm_bin=$2
+  local output=$3
+
+  [[ -f "$x86_bin" ]] && [[ -f "$arm_bin" ]] || { echo "Missing input binaries for $output"; return 1; }
+
+  lipo -create -arch x86_64 "$x86_bin" -arch arm64 "$arm_bin" -output futurerestore
+  codesign --force --sign - futurerestore
+  tar cpPJf "$output" futurerestore
+  rm -f futurerestore
+}
+
+package cmake-build-release-x86_64/src/futurerestore \
+        cmake-build-release-arm64/src/futurerestore \
+        "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-RELEASE.tar.xz"
+
+package cmake-build-debug-x86_64/src/futurerestore \
+        cmake-build-debug-arm64/src/futurerestore \
+        "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-DEBUG.tar.xz"
+
+package cmake-build-asan-x86_64/src/futurerestore \
+        cmake-build-asan-arm64/src/futurerestore \
+        "futurerestore-macOS-${FUTURERESTORE_VERSION_RELEASE}-Build_${FUTURERESTORE_VERSION}-ASAN.tar.xz"
